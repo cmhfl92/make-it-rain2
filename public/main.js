4a2ee8f8eb502a57373d212cc6bc899fe8fed8f3
Christa Hegedus <Christa.Hegedus1@gmail.com>
2016-11-27 21:07:10 -0500

    finished most of HTML/CSS, tried some JS

--- public/main.js
+++ public/main.js
@@ -1,6 +1,29 @@
+const qs = s => document.querySelector(s)
+
+
 const main = () => {
-  document.getElementById('root').textContent = 'Hello, World!'
+  const rain =
+  document.querySelector('.rain')
+
+  rain.style.top = '0px'
+
+  setInterval(() => {
+    rain.style.top = '0px'
+  }, 60000)
+
+  const animate = () => {
+    rain.style.top =
+   `{parseInt(rain.style.top) + 1}px`
+  }
+
+  setInterval(animate, 12)
+
+ const rain () =>
+  clearInterval('.rain')
+
+  setTimeout(() => {
+    rain.style.backgroundColor = 'lightblue'
+  }, 5000)
 }
 
 document.addEventListener('DOMContentLoaded', main)
-
